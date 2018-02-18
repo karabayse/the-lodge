9bcb4c085e72c1aeb9dbe37fa6ffdeca2f2081fe
Kara Bayse <karabayse@gmail.com>
2018-02-18 16:10:23 -0600

    server up

---
+++ server.js
@@ -0,0 +1,23 @@
+// requires
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// uses
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/views/index.html'));
+});
+
+// globals
+var port = process.env.PORT || 4444;
+
+// spin up server
+app.listen(port, function() {
+  console.log('server up on:', port);
+});
